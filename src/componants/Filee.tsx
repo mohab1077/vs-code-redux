4c952af572feb06f11134f7e15f3cf2b86c6b2b5
mohabel <mohab1077@gmail.com>
2025-08-11 02:21:47 +0300

    added code highlight

--- src/componants/Filee.tsx
+++ src/componants/Filee.tsx
@@ -1,16 +1,25 @@
 import { useSelector } from "react-redux"
 import type { RootState } from "../app/store"
-
+import hljs from "highlight.js";
+import "highlight.js/styles/atom-one-dark.css";
 
 
 function Filee() {
 
   const data = useSelector((state: RootState) => state.file.clickedfile)
-
+  const content = data?.content ?? "";
+   const highlightedCode = hljs.highlightAuto(content).value;
+  
   return (
-    <div className={`bg-black w-full h-screen ${!data ? 'flex justify-center items-center' : ''}`}>
+    <div className={`bg-gray-900 w-full h-screen ${!data ? 'flex justify-center items-center' : ''}`}>
       {data ?
-        <p className="text-amber-100">{data.content}</p>
+      
+        <pre>
+        <code
+          className="text-amber-100"
+          dangerouslySetInnerHTML={{ __html: highlightedCode }}
+        />
+      </pre>
         :
         <img src="/icons/vscode.svg" alt="" className='h-56 w-48' />
       }
